8bfdf38eaca3a34c93f0cae37d5a0494cb2da9a3
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-09 20:12:35 +0300

    a primitive language for creating drawings

---
+++ proto.h
@@ -0,0 +1,16 @@
+#ifndef __ASCII_CAD_PROTO_H_INCLUDED__
+#define __ASCII_CAD_PROTO_H_INCLUDED__
+
+#define SIZE 32
+
+int get_brush_c();
+void interpret(char*);
+
+void reta(char*),
+	 rect(char*),
+	 point(char*),
+	 slant(char*),
+	 set_brush(char*),
+	 sym(char*);
+
+#endif

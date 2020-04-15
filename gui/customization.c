8c2c66278c169d8597ca52932bfa06de361ada79
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-15 15:17:54 +0300

    full customization

--- gui/customization.c
+++ gui/customization.c
@@ -8,3 +8,74 @@ void ppm_apply_click(GtkWidget *bttn, GtkWidget *entry) {
 	if (new_ppm > 0)
 		uni_set("ppm", new_ppm);
 }
+
+void sg_apply_click(GtkWidget *bttn, GtkWidget *entry) {
+	int is_show_grid = atoi(gtk_entry_get_text(GTK_ENTRY(entry)));
+	uni_set("sg", is_show_grid);
+}
+
+void fc_apply_click(GtkWidget *bttn, GtkWidget *entry) {
+	char theme[256] = "";
+	char *red = NULL, *green = NULL, *blue = NULL;
+
+	strcpy(theme, (char*)gtk_entry_get_text(GTK_ENTRY(entry)));
+
+	red = strtok(theme, " ");
+	green = strtok(NULL, " ");
+	blue = strtok(NULL, " ");
+
+	uni_set("fr", atof(red));
+	uni_set("fg", atof(green));
+	uni_set("fb", atof(blue));
+}
+
+void bc_apply_click(GtkWidget *bttn, GtkWidget *entry) {
+	char theme[256] = "";
+	char *red = NULL, *green = NULL, *blue = NULL;
+
+	strcpy(theme, (char*)gtk_entry_get_text(GTK_ENTRY(entry)));
+
+	red = strtok(theme, " ");
+	green = strtok(NULL, " ");
+	blue = strtok(NULL, " ");
+
+	uni_set("br", atof(red));
+	uni_set("bg", atof(green));
+	uni_set("bb", atof(blue));
+}
+
+void gc_apply_click(GtkWidget *bttn, GtkWidget *entry) {
+	char theme[256] = "";
+	char *red = NULL, *green = NULL, *blue = NULL;
+
+	strcpy(theme, (char*)gtk_entry_get_text(GTK_ENTRY(entry)));
+
+	red = strtok(theme, " ");
+	green = strtok(NULL, " ");
+	blue = strtok(NULL, " ");
+
+	uni_set("gfr", atof(red));
+	uni_set("gfg", atof(green));
+	uni_set("gfb", atof(blue));
+}
+
+void lw_apply_click(GtkWidget *bttn, GtkWidget *entry) {
+	double new_width = atof(gtk_entry_get_text(GTK_ENTRY(entry)));
+
+	if (new_width > 0)
+		uni_set("th", new_width);	
+}
+
+void gw_apply_click(GtkWidget *bttn, GtkWidget *entry) {
+	double new_width = atof(gtk_entry_get_text(GTK_ENTRY(entry)));
+
+	if (new_width > 0)
+		uni_set("gt", new_width);	
+}
+
+void pr_apply_click(GtkWidget *bttn, GtkWidget *entry) {
+	double new_radius = atof(gtk_entry_get_text(GTK_ENTRY(entry)));
+
+	if (new_radius > 0)
+		uni_set("pr", new_radius);	
+}

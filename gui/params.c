b6dcf6bec8acd70bf942933dba467d86bcf66136
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-15 15:18:24 +0300

    full customization

--- gui/params.c
+++ gui/params.c
@@ -1,6 +1,10 @@
 #include <string.h>
+#include <stdlib.h>
+#include "proto.h"
 
-double PIX_PER_MM = 5;
+#define LEN 14
+
+double PIX_PER_MM = 5.0;
 double grid_fg_red = 0.2, grid_fg_green = 0.2, grid_fg_blue = 0.2;
 double grid_thick = 1;
 double point_r = 4;
@@ -10,20 +14,19 @@ double fg_red = 0, fg_green = 0.5, fg_blue = 1;
 
 double thick = 2;
 
-double uni_get(char *param) {
-	char *params[] = {"ppm", "gfr", "gfg", "gfb", "gt", "pr", "br", "bg", "bb", "fr", "fg", "fb", "th"};
-	double *values[] = {&PIX_PER_MM, &grid_fg_red, &grid_fg_green, &grid_fg_blue, &grid_thick, &point_r, &bg_red, &bg_green, &bg_blue, &fg_red, &fg_green, &fg_blue, &thick};
+double show_grid = 1.0f;
 
-	for (int i = 0; i < 13; i++)
+char *params[] = {"ppm", "gfr", "gfg", "gfb", "gt", "pr", "br", "bg", "bb", "fr", "fg", "fb", "th", "sg"};
+double *values[] = {&PIX_PER_MM, &grid_fg_red, &grid_fg_green, &grid_fg_blue, &grid_thick, &point_r, &bg_red, &bg_green, &bg_blue, &fg_red, &fg_green, &fg_blue, &thick, &show_grid};
+
+double uni_get(char *param) {
+	for (int i = 0; i < LEN; i++)
 		if (!strcmp(params[i], param)) return *(values[i]);
 	return 0;
 }
 
 void uni_set(char *param, double new_value) {
-	char *params[] = {"ppm", "gfr", "gfg", "gfb", "gt", "pr", "br", "bg", "bb", "fr", "fg", "fb", "th"};
-	double *values[] = {&PIX_PER_MM, &grid_fg_red, &grid_fg_green, &grid_fg_blue, &grid_thick, &point_r, &bg_red, &bg_green, &bg_blue, &fg_red, &fg_green, &fg_blue, &thick};
-
-	for (int i = 0; i < 13; i++) {
+	for (int i = 0; i < LEN; i++) {
 		if (!strcmp(params[i], param)) {
 			*(values[i]) = new_value;
 			break;

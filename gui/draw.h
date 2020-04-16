4635bcab95dda5306e6587754524fdafeb3a48a7
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-16 13:30:02 +0300

    arc added

--- gui/draw.h
+++ gui/draw.h
@@ -1,13 +1,14 @@
 #ifndef __GUI_INTERPRETER_FOR_BLUE_PRINT_LANGUAGE_H_INCLUDED__
 #define __GUI_INTERPRETER_FOR_BLUE_PRINT_LANGUAGE_H_INCLUDED__
 
-void draw_line(cairo_t *cr, double x1, double y1, double x2, double y2);
-void draw_grid(cairo_t *cr, guint width, guint height, guint step);
-void draw_point(cairo_t *cr, double x, double y);
-void draw_rect(cairo_t *cr, double x, double y, double width, double height);
-void draw_circle(cairo_t *cr, double x, double y, double radii);
-void draw_text(cairo_t *cr, const char *text, double x, double y, int size);
+void draw_line(cairo_t*, double, double, double, double);
+void draw_grid(cairo_t*, guint, guint, guint);
+void draw_point(cairo_t*, double, double);
+void draw_rect(cairo_t*, double, double, double, double);
+void draw_circle(cairo_t*, double, double, double);
+void draw_text(cairo_t*, const char*, double, double, int);
+void draw_arc(cairo_t*, double, double, double, double, double);
 
-double mm_to_pix(double mm);
+double mm_to_pix(double);
 
 #endif

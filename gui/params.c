651b60529a4e47ffd4d4e395ff8d79bcd1d9cb08
Centrix14 <artiom.zvyagin@yandex.ru>
2020-05-04 14:18:54 +0300

    new cust param: grid step

--- gui/params.c
+++ gui/params.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include "proto.h"
 
-#define LEN 14
+#define LEN 15
 
 double PIX_PER_MM = 5.0;
 double grid_fg_red = 0.2, grid_fg_green = 0.2, grid_fg_blue = 0.2;
-double grid_thick = 1;
+double grid_thick = 1, grid_step = 5;
 double point_r = 4;
 
 double bg_red = 0, bg_green = 0, bg_blue = 0;
@@ -16,8 +16,8 @@ double thick = 2;
 
 double show_grid = 1.0f;
 
-char *params[] = {"ppm", "gfr", "gfg", "gfb", "gt", "pr", "br", "bg", "bb", "fr", "fg", "fb", "th", "sg"};
-double *values[] = {&PIX_PER_MM, &grid_fg_red, &grid_fg_green, &grid_fg_blue, &grid_thick, &point_r, &bg_red, &bg_green, &bg_blue, &fg_red, &fg_green, &fg_blue, &thick, &show_grid};
+char *params[] = {"ppm", "gfr", "gfg", "gfb", "gt", "pr", "br", "bg", "bb", "fr", "fg", "fb", "th", "sg", "gs"};
+double *values[] = {&PIX_PER_MM, &grid_fg_red, &grid_fg_green, &grid_fg_blue, &grid_thick, &point_r, &bg_red, &bg_green, &bg_blue, &fg_red, &fg_green, &fg_blue, &thick, &show_grid, &grid_step};
 
 double uni_get(char *param) {
 	for (int i = 0; i < LEN; i++)
